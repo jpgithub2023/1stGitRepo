e4a5f98358914bf1cf2c6f7ca40ec6b15340a7e4
jpgithub2023 <134462870+jpgithub2023@users.noreply.github.com>
2023-07-09 21:34:15 -0400

    committed 2

---
+++ src/launchBrowser/gitTest2.java
@@ -0,0 +1,17 @@
+package launchBrowser;
+
+public class gitTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("git test");
+		printMe();
+		
+	}
+	
+	public static void printMe() {
+	
+		System.out.println("git test1");
+		System.out.println("git test2");
+		System.out.println("git test3");
+	}
+}
